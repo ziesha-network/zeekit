3ff7629f96095a84bb57e4ec75dec78a360a02f4
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-04-26 16:57:23 +0430

    Bridge BlsScalar and Fr

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,28 @@
+use dusk_plonk::prelude::*;
+use ff::PrimeField;
+use serde::{Deserialize, Serialize};
+
 #[macro_use]
 extern crate lazy_static;
+
+// Scalar field of Bls12-381
+#[derive(PrimeField, Serialize, Deserialize)]
+#[PrimeFieldModulus = "52435875175126190479447740508185965837690552500527637822603658699938581184513"]
+#[PrimeFieldGenerator = "7"]
+#[PrimeFieldReprEndianness = "little"]
+pub struct Fr([u64; 4]);
+
+impl Into<BlsScalar> for Fr {
+    fn into(self) -> BlsScalar {
+        BlsScalar(self.0)
+    }
+}
+
+impl From<BlsScalar> for Fr {
+    fn from(bls: BlsScalar) -> Fr {
+        Fr(bls.0)
+    }
+}
 
 mod config;
 pub mod eddsa;
