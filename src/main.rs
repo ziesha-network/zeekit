267d45285424827c5393ef469d6fa96db34d5b9e
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-05-10 01:55:32 +0430

    Test point mul

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use bellman::gadgets::num::AllocatedNum;
 use bellman::{groth16, Circuit, ConstraintSystem, SynthesisError};
 use bls12_381::{Bls12, Scalar as BellmanFr};
-use ff::Field;
 use rand::rngs::OsRng;
-use zeekit::{mimc, Fr};
+use zeekit::{eddsa, Fr};
 
 #[derive(Clone)]
 pub struct DummyDemo {
-    xl: Option<BellmanFr>,
-    xr: Option<BellmanFr>,
-    hash: Option<BellmanFr>,
+    curve_a: Option<BellmanFr>,
+    curve_d: Option<BellmanFr>,
+    a: (Option<BellmanFr>, Option<BellmanFr>),
+    mul: Option<BellmanFr>,
+    c: (Option<BellmanFr>, Option<BellmanFr>),
 }
 
 impl Circuit<BellmanFr> for DummyDemo {
@@ -17,24 +18,60 @@ impl Circuit<BellmanFr> for DummyDemo {
         self,
         cs: &mut CS,
     ) -> Result<(), SynthesisError> {
-        let xl = AllocatedNum::alloc(&mut *cs, || {
-            self.xl.ok_or(SynthesisError::AssignmentMissing)
+        let curve_a = AllocatedNum::alloc(&mut *cs, || {
+            self.curve_a.ok_or(SynthesisError::AssignmentMissing)
         })?;
-        let xr = AllocatedNum::alloc(&mut *cs, || {
-            self.xr.ok_or(SynthesisError::AssignmentMissing)
+        curve_a.inputize(&mut *cs)?;
+        let curve_d = AllocatedNum::alloc(&mut *cs, || {
+            self.curve_d.ok_or(SynthesisError::AssignmentMissing)
         })?;
-        let claimed_hash = AllocatedNum::alloc(&mut *cs, || {
-            self.hash.ok_or(SynthesisError::AssignmentMissing)
-        })?;
-        claimed_hash.inputize(&mut *cs)?;
+        curve_d.inputize(&mut *cs)?;
 
-        let hash = mimc::groth16::double_mimc(&mut *cs, xl, xr)?;
+        let a_x = AllocatedNum::alloc(&mut *cs, || {
+            self.a.0.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        a_x.inputize(&mut *cs)?;
+
+        let a_y = AllocatedNum::alloc(&mut *cs, || {
+            self.a.1.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        a_y.inputize(&mut *cs)?;
+
+        let mul = AllocatedNum::alloc(&mut *cs, || {
+            self.mul.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        mul.inputize(&mut *cs)?;
+
+        let c_x = AllocatedNum::alloc(&mut *cs, || {
+            self.c.0.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        c_x.inputize(&mut *cs)?;
+
+        let c_y = AllocatedNum::alloc(&mut *cs, || {
+            self.c.1.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        c_y.inputize(&mut *cs)?;
+
+        let calc = eddsa::groth16::mul_point(
+            &mut *cs,
+            curve_a,
+            curve_d,
+            eddsa::groth16::AllocatedPoint { x: a_x, y: a_y },
+            mul,
+        )?;
 
         cs.enforce(
             || "",
-            |lc| lc + hash.get_variable(),
+            |lc| lc + calc.x.get_variable(),
             |lc| lc + CS::one(),
-            |lc| lc + claimed_hash.get_variable(),
+            |lc| lc + c_x.get_variable(),
+        );
+
+        cs.enforce(
+            || "",
+            |lc| lc + calc.y.get_variable(),
+            |lc| lc + CS::one(),
+            |lc| lc + c_y.get_variable(),
         );
 
         Ok(())
@@ -44,28 +81,45 @@ impl Circuit<BellmanFr> for DummyDemo {
 fn main() {
     let params = {
         let c = DummyDemo {
-            xl: None,
-            xr: None,
-            hash: None,
+            curve_a: None,
+            curve_d: None,
+            a: (None, None),
+            mul: None,
+            c: (None, None),
         };
         groth16::generate_random_parameters::<Bls12, _, _>(c, &mut OsRng).unwrap()
     };
 
     let pvk = groth16::prepare_verifying_key(&params.vk);
 
-    let xl = Fr::zero();
-    let xr = Fr::one();
-    let hash = mimc::double_mimc(xl, xr);
+    let curve_a = eddsa::A.clone();
+    let curve_d = eddsa::D.clone();
+    let a = eddsa::BASE.clone();
+    let mul = Fr::from(1234567);
+    let c = a.multiply(&mul);
 
-    let c = DummyDemo {
-        xl: Some(xl.into()),
-        xr: Some(xr.into()),
-        hash: Some(hash.into()),
+    let circ = DummyDemo {
+        curve_a: Some(curve_a.into()),
+        curve_d: Some(curve_d.into()),
+        a: (Some(a.0.into()), Some(a.1.into())),
+        mul: Some(mul.into()),
+        c: (Some(c.0.into()), Some(c.1.into())),
     };
 
-    let proof = groth16::create_random_proof(c, &params, &mut OsRng).unwrap();
+    let proof = groth16::create_random_proof(circ, &params, &mut OsRng).unwrap();
 
-    let inputs = vec![hash.into()];
+    let inputs = vec![
+        curve_a.into(),
+        curve_d.into(),
+        a.0.into(),
+        a.1.into(),
+        Fr::from(1234567).into(),
+        c.0.into(),
+        c.1.into(),
+    ];
 
-    println!("Verify: {}",groth16::verify_proof(&pvk, &proof, &inputs).is_ok());
+    println!(
+        "Verify: {}",
+        groth16::verify_proof(&pvk, &proof, &inputs).is_ok()
+    );
 }
