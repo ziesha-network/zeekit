081923247fdbaf84dcb6550becb76ba4081ac6ed
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-05-09 18:18:12 +0430

    Implement merkle-proof check for groth16

--- src/merkle/groth16.rs
+++ src/merkle/groth16.rs
@@ -1,1 +1,82 @@
+use crate::mimc;
+use crate::BellmanFr;
 
+use bellman::gadgets::boolean::{AllocatedBit, Boolean};
+use bellman::gadgets::num::AllocatedNum;
+use bellman::{ConstraintSystem, SynthesisError};
+
+fn merge_hash<'a, CS: ConstraintSystem<BellmanFr>>(
+    cs: &mut CS,
+    select: Boolean,
+    a: AllocatedNum<BellmanFr>,
+    b: AllocatedNum<BellmanFr>,
+) -> Result<AllocatedNum<BellmanFr>, SynthesisError> {
+    let l = mimc::groth16::double_mimc(cs, a.clone(), b.clone())?;
+    let r = mimc::groth16::double_mimc(cs, b.clone(), a.clone())?;
+    let (l, _) = AllocatedNum::conditionally_reverse(&mut *cs, &l, &r, &select)?;
+    Ok(l)
+}
+
+pub fn calc_root<'a, CS: ConstraintSystem<BellmanFr>>(
+    cs: &mut CS,
+    index: AllocatedNum<BellmanFr>,
+    val: AllocatedNum<BellmanFr>,
+    proof: Vec<AllocatedNum<BellmanFr>>,
+) -> Result<AllocatedNum<BellmanFr>, SynthesisError> {
+    let selectors = index.to_bits_le(&mut *cs)?;
+    let mut curr = val.clone();
+    for (p, dir) in proof.into_iter().zip(selectors.into_iter()) {
+        curr = merge_hash(&mut *cs, dir, curr, p)?;
+    }
+    Ok(curr)
+}
+
+fn assert_equal<'a, CS: ConstraintSystem<BellmanFr>>(
+    cs: &mut CS,
+    enabled: AllocatedBit,
+    a: AllocatedNum<BellmanFr>,
+    b: AllocatedNum<BellmanFr>,
+) -> Result<(), SynthesisError> {
+    let enabled_value = enabled.get_value();
+    let enabled_in_a = cs.alloc(
+        || "",
+        || {
+            enabled_value
+                .map(|e| {
+                    if e {
+                        a.get_value()
+                    } else {
+                        Some(BellmanFr::zero())
+                    }
+                })
+                .unwrap_or(None)
+                .ok_or(SynthesisError::AssignmentMissing)
+        },
+    )?;
+    cs.enforce(
+        || "enabled * a == enabled_in_a",
+        |lc| lc + enabled.get_variable(),
+        |lc| lc + a.get_variable(),
+        |lc| lc + enabled_in_a,
+    );
+    cs.enforce(
+        || "enabled * b == enabled_in_a",
+        |lc| lc + enabled.get_variable(),
+        |lc| lc + b.get_variable(),
+        |lc| lc + enabled_in_a,
+    );
+    Ok(())
+}
+
+pub fn check_proof<'a, CS: ConstraintSystem<BellmanFr>>(
+    cs: &mut CS,
+    enabled: AllocatedBit,
+    index: AllocatedNum<BellmanFr>,
+    val: AllocatedNum<BellmanFr>,
+    proof: Vec<AllocatedNum<BellmanFr>>,
+    root: AllocatedNum<BellmanFr>,
+) -> Result<(), SynthesisError> {
+    let new_root = calc_root(&mut *cs, index, val, proof)?;
+    assert_equal(cs, enabled, root, new_root)?;
+    Ok(())
+}
--- src/mimc/groth16.rs
+++ src/mimc/groth16.rs
@@ -2,19 +2,20 @@ use super::MIMC_PARAMS;
 use crate::BellmanFr;
 use std::ops::*;
 
-use bellman::{ConstraintSystem, SynthesisError, Variable};
+use bellman::gadgets::num::AllocatedNum;
+use bellman::{ConstraintSystem, SynthesisError};
 
 pub fn double_mimc<'a, CS: ConstraintSystem<BellmanFr>>(
     cs: &mut CS,
-    mut xl: (Variable, Option<BellmanFr>),
-    mut xr: (Variable, Option<BellmanFr>),
-) -> Result<(Variable, Option<BellmanFr>), SynthesisError> {
+    mut xl: AllocatedNum<BellmanFr>,
+    mut xr: AllocatedNum<BellmanFr>,
+) -> Result<AllocatedNum<BellmanFr>, SynthesisError> {
     for (i, c) in MIMC_PARAMS.iter().cloned().enumerate() {
         // xL, xR := xR + (xL + Ci)^3, xL
         let cs = &mut cs.namespace(|| format!("round {}", i));
 
         // tmp = (xL + Ci)^2
-        let tmp_value = xl.1.map(|mut e| {
+        let tmp_value = xl.get_value().map(|mut e| {
             e.add_assign(&c.into());
             e.square()
         });
@@ -25,34 +26,30 @@ pub fn double_mimc<'a, CS: ConstraintSystem<BellmanFr>>(
 
         cs.enforce(
             || "tmp = (xL + Ci)^2",
-            |lc| lc + xl.0 + (c.into(), CS::one()),
-            |lc| lc + xl.0 + (c.into(), CS::one()),
+            |lc| lc + xl.get_variable() + (c.into(), CS::one()),
+            |lc| lc + xl.get_variable() + (c.into(), CS::one()),
             |lc| lc + tmp,
         );
 
         // new_xL = xR + (xL + Ci)^3
         // new_xL = xR + tmp * (xL + Ci)
         // new_xL - xR = tmp * (xL + Ci)
-        let new_xl_value = xl.1.map(|mut e| {
+        let new_xl_value = xl.get_value().map(|mut e| {
             e.add_assign(&c.into());
             e.mul_assign(&tmp_value.unwrap());
-            e.add_assign(&xr.1.unwrap());
+            e.add_assign(&xr.get_value().unwrap());
             e
         });
 
-        let new_xl = (
-            cs.alloc(
-                || "new_xl",
-                || new_xl_value.ok_or(SynthesisError::AssignmentMissing),
-            )?,
-            new_xl_value,
-        );
+        let new_xl = AllocatedNum::alloc(&mut *cs, || {
+            new_xl_value.ok_or(SynthesisError::AssignmentMissing)
+        })?;
 
         cs.enforce(
             || "new_xL = xR + (xL + Ci)^3",
             |lc| lc + tmp,
-            |lc| lc + xl.0 + (c.into(), CS::one()),
-            |lc| lc + new_xl.0 - xr.0,
+            |lc| lc + xl.get_variable() + (c.into(), CS::one()),
+            |lc| lc + new_xl.get_variable() - xr.get_variable(),
         );
 
         // xR = xL
@@ -67,12 +64,12 @@ pub fn double_mimc<'a, CS: ConstraintSystem<BellmanFr>>(
 
 pub fn mimc<'a, CS: ConstraintSystem<BellmanFr>>(
     cs: &mut CS,
-    data: &[(Variable, Option<BellmanFr>)],
-) -> Result<(Variable, Option<BellmanFr>), SynthesisError> {
+    data: &[AllocatedNum<BellmanFr>],
+) -> Result<AllocatedNum<BellmanFr>, SynthesisError> {
     assert!(data.len() >= 2);
-    let mut accum = double_mimc(cs, data[0], data[1])?;
+    let mut accum = double_mimc(cs, data[0].clone(), data[1].clone())?;
     for w in data[2..].iter() {
-        accum = double_mimc(cs, accum, *w)?;
+        accum = double_mimc(cs, accum, w.clone())?;
     }
     Ok(accum)
 }
