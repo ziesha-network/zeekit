ec7ca239b803aab5236ec1e884ca6e5f6d867b42
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-05-14 16:56:21 +0430

    Handle larger than modulus

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use ff::PrimeField;
+use num_bigint::BigUint;
+use num_integer::Integer;
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 
 #[cfg(feature = "plonk")]
 use dusk_plonk::prelude::BlsScalar;
@@ -10,6 +13,13 @@ pub use bls12_381::Scalar as BellmanFr;
 #[macro_use]
 extern crate lazy_static;
 
+lazy_static! {
+    static ref FR_MODULUS: BigUint = BigUint::from_str(
+        "52435875175126190479447740508185965837690552500527637822603658699938581184513"
+    )
+    .unwrap();
+}
+
 // Scalar field of Bls12-381
 #[derive(PrimeField, Serialize, Deserialize)]
 #[PrimeFieldModulus = "52435875175126190479447740508185965837690552500527637822603658699938581184513"]
@@ -19,7 +29,12 @@ pub struct Fr([u64; 4]);
 
 impl Fr {
     pub fn new(num_le: [u8; 32]) -> Self {
-        Fr::from_repr_vartime(FrRepr(num_le)).unwrap()
+        let bts = BigUint::from_bytes_le(&num_le)
+            .mod_floor(&FR_MODULUS)
+            .to_bytes_le();
+        let mut data = [0u8; 32];
+        data[0..bts.len()].copy_from_slice(&bts);
+        Fr::from_repr_vartime(FrRepr(data)).unwrap()
     }
 }
 
