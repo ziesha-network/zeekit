be5a363cdadb26f42650d369fc2e771fd77af2d4
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-05-23 20:22:15 +0430

    Add groth16 verifier

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use std::str::FromStr;
 use dusk_plonk::prelude::BlsScalar;
 
 #[cfg(feature = "groth16")]
-pub use bls12_381::Scalar as BellmanFr;
+pub use bls12_381::{Bls12, G1Affine as BellmanG1, G2Affine as BellmanG2, Scalar as BellmanFr};
 
 #[macro_use]
 extern crate lazy_static;
@@ -26,6 +26,65 @@ lazy_static! {
 #[PrimeFieldGenerator = "7"]
 #[PrimeFieldReprEndianness = "little"]
 pub struct Fr([u64; 4]);
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Fp([u64; 6]);
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Groth16VerifyingKey {
+    alpha_g1: (Fp, Fp, bool),
+    beta_g1: (Fp, Fp, bool),
+    beta_g2: ((Fp, Fp), (Fp, Fp), bool),
+    gamma_g2: ((Fp, Fp), (Fp, Fp), bool),
+    delta_g1: (Fp, Fp, bool),
+    delta_g2: ((Fp, Fp), (Fp, Fp), bool),
+    ic: Vec<(Fp, Fp, bool)>,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Groth16Proof {
+    a: (Fp, Fp, bool),
+    b: ((Fp, Fp), (Fp, Fp), bool),
+    c: (Fp, Fp, bool),
+}
+
+#[cfg(feature = "groth16")]
+pub fn groth16_verify(vk: &Groth16VerifyingKey, proof: &Groth16Proof) -> bool {
+    let (vk, proof) = unsafe {
+        let alpha_g1 = std::mem::transmute::<(Fp, Fp, bool), BellmanG1>(vk.alpha_g1.clone());
+        let beta_g1 = std::mem::transmute::<(Fp, Fp, bool), BellmanG1>(vk.beta_g1.clone());
+        let beta_g2 =
+            std::mem::transmute::<((Fp, Fp), (Fp, Fp), bool), BellmanG2>(vk.beta_g2.clone());
+        let gamma_g2 =
+            std::mem::transmute::<((Fp, Fp), (Fp, Fp), bool), BellmanG2>(vk.gamma_g2.clone());
+        let delta_g1 = std::mem::transmute::<(Fp, Fp, bool), BellmanG1>(vk.delta_g1.clone());
+        let delta_g2 =
+            std::mem::transmute::<((Fp, Fp), (Fp, Fp), bool), BellmanG2>(vk.delta_g2.clone());
+        let ic = vk
+            .ic
+            .iter()
+            .cloned()
+            .map(|p| std::mem::transmute::<(Fp, Fp, bool), BellmanG1>(p))
+            .collect();
+        let proof = bellman::groth16::Proof::<Bls12> {
+            a: std::mem::transmute::<(Fp, Fp, bool), BellmanG1>(proof.a.clone()),
+            b: std::mem::transmute::<((Fp, Fp), (Fp, Fp), bool), BellmanG2>(proof.b.clone()),
+            c: std::mem::transmute::<(Fp, Fp, bool), BellmanG1>(proof.c.clone()),
+        };
+        let vk =
+            bellman::groth16::prepare_verifying_key(&bellman::groth16::VerifyingKey::<Bls12> {
+                alpha_g1,
+                beta_g1,
+                beta_g2,
+                gamma_g2,
+                delta_g1,
+                delta_g2,
+                ic,
+            });
+        (vk, proof)
+    };
+    bellman::groth16::verify_proof(&vk, &proof, &vec![]).is_ok()
+}
 
 impl Fr {
     pub fn new(num_le: [u8; 32]) -> Self {
